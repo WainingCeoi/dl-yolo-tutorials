6b5995439fc8d5cf5cdbf978d314656c09780151
Weining <weiningxu1018@gmail.com>
2025-10-21 19:43:30 +0800

    Lecture 2: Yolo with Webcam

--- yolo/L2_Yolo_w_Webcam.py
+++ yolo/L2_Yolo_w_Webcam.py
@@ -4,19 +4,20 @@ from ultralytics import YOLO
 import cv2
 
 
-cap = cv2.VideoCapture(0)
+# For Webcam
+cap = cv2.VideoCapture(1) # 0 for Mac, 1 for iPhone (Continuity Camera)
 cap.set(3, 1280)
 cap.set(4, 720)
+
+# For Video
+# cap = cv2.VideoCapture("Videos/bikes.mp4")
 
 model = YOLO("Yolo_Models/yolo11s.pt")
 
 while True:
     success, frame = cap.read()
-    results = model(frame, device="mps", stream=True)
+    results = model(frame, stream=True)
     for result in results:
-        cv2.imshow("Image", result.plot())
+        cv2.imshow("YOLO Detection Model (Press \"Q\" to Quit)", result.plot())
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-cap.release()
-cv2.destroyAllWindows()
