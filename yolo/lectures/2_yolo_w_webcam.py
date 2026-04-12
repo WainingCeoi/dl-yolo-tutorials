e8e1c117f686966404fb6eac6d41ec49cf415431
WeiningXu <weiningxu1018@gmail.com>
2026-04-12 23:51:47 +0800

    Lecture 2: Yolo with Webcam

--- yolo/lectures/2_yolo_w_webcam.py
+++ yolo/lectures/2_yolo_w_webcam.py
@@ -11,14 +11,14 @@ cap.set(4, 720)
 # For Video
 # cap = cv2.VideoCapture("Videos/bikes.mp4")
 
-model = YOLO("Yolo_Models/yolo11s.pt")
+model = YOLO("../yolo_models/yolo11s.pt")
 
 while cap.isOpened():
     success, frame = cap.read()
     if not success:
         print("Video frame is empty or Processing is complete.")
         break
-    results = model(frame, stream=True, device="mps")
+    results = model(frame, stream=True)
     for result in results:
         cv2.imshow("YOLO Detection Model (Press \"Q\" to Quit)", result.plot())
     if cv2.waitKey(1) == ord('q'):
