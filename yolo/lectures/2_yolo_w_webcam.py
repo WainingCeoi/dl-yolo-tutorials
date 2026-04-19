4aab925fd90d9a210b07b1a484a57b8b660c904b
WeiningXu <weiningxu1018@gmail.com>
2026-04-19 21:17:03 +0800

    Updated webcam engine

--- yolo/lectures/2_yolo_w_webcam.py
+++ yolo/lectures/2_yolo_w_webcam.py
@@ -1,28 +1,10 @@
 """Lecture 2: Yolo with Webcam"""
 from ultralytics import YOLO
-import cv2
 
 
 # For Webcam
-cap = cv2.VideoCapture(0) # 0 for Mac, 1 for iPhone (Continuity Camera)
-cap.set(3, 1280)
-cap.set(4, 720)
-
-# For Video
-cap = cv2.VideoCapture("../videos/bikes.mp4")
+video_source =0 # 0 for Mac, 1 for iPhone (Continuity Camera)
 
 model = YOLO("../yolo_models/yolo26s.pt")
 
-while cap.isOpened():
-    success, frame = cap.read()
-    if not success:
-        print("Video frame is empty or Processing is complete.")
-        break
-    results = model(frame, stream=True, device="cpu")
-    for result in results:
-        cv2.imshow("YOLO Detection Model (Press \"Q\" to Quit)", result.plot())
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyWindow()
+result = model.predict(source=0, show=True, save=False, device="cpu")
