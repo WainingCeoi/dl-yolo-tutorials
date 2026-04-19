63d58df4aeda2b265fc334dcb5f16b7d3016d565
WeiningXu <weiningxu1018@gmail.com>
2026-04-19 21:23:54 +0800

    Project 3: PPE Detector (Training)

--- yolo/projects/3.1_ppe_detector_training.py
+++ yolo/projects/3.1_ppe_detector_training.py
@@ -8,6 +8,7 @@ model = YOLO("../yolo_models/yolo26n.pt")
 results = model.train(
     data="../datasets/construction_site_safety/data.yaml",
     epochs=5,
+    name="ppe_detector",
     imgsz=640,
     workers = 6,
     device = "cpu",
