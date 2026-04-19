d17e945181b48df91ef5c4beaa2c7b46358d6a7d
WeiningXu <weiningxu1018@gmail.com>
2026-04-19 21:24:05 +0800

    Project 3: PPE Detector (Deploy)

--- yolo/projects/3.2_ppe_detector_deploy.py
+++ yolo/projects/3.2_ppe_detector_deploy.py
@@ -9,6 +9,6 @@ video_source = 0 # 0 for Mac, 1 for iPhone (Continuity Camera)
 # For video input
 # video_source = "../videos/ppe-3-1.mp4"
 
-model = YOLO("../trained_models/ppe_detector/train/weights/best.pt")
+model = YOLO("../trained_models/ppe_detector/weights/best.pt")
 
 result = model.predict(source=video_source, show=True, save=False, device="cpu")
