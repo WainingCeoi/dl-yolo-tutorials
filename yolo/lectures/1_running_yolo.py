690b646ee89d75df8de67678550530fb6f7568c3
WeiningXu <weiningxu1018@gmail.com>
2026-04-20 15:30:06 +0800

    Used new yolo model

--- yolo/lectures/1_running_yolo.py
+++ yolo/lectures/1_running_yolo.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 
 
 model = YOLO("../yolo_models/yolo26s.pt")
-results = model("../images/1.jpg")[0].show()
+results = model.predict("../datasets/images/1.jpg", save=False, device="cpu")[0].show()
